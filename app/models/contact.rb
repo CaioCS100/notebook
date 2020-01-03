7d619263b9327ce3d36f3b88fe452dbbbfff7486
Caio Cesar Silva <caio.cesar.s100@gmail.com>
2020-01-03 09:38:54 -0300

    Adding I18n to birthdate and creating a new method that transform the birthdate field to language br

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,28 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+  belongs_to :kind
+
+  def to_br
+    {
+      id: id,
+      name: name,
+      email: email,
+      birthdate: I18n.l(birthdate),
+      kind: {
+        id: kind.id,
+        description: kind.description
+      }
+    }
+  end
+
+#   def name_author
+#     'Caio Cesar Silva'
+#   end
+
+#   def as_json(options = {})
+#     super({
+#         root: true,
+#         methods: :name_author,
+#         include: {kind: { only: [:id, :description] }}
+#     })
+#   end
 end
